158c3180f7f61c0fb803e2754b75a88b8a65a6a8
FranciscoChamba <francisco.chamba@unl.edu.ec>
2026-02-05 23:56:53 -0500

    Implementar Clases Repository

--- src/main/java/edu/unl/cc/ordermaster/business/service/common/MenuRepository.java
+++ src/main/java/edu/unl/cc/ordermaster/business/service/common/MenuRepository.java
@@ -1,4 +1,31 @@
 package edu.unl.cc.ordermaster.business.service.common;
 
+import edu.unl.cc.ordermaster.business.service.CrudGenericService;
+import edu.unl.cc.ordermaster.domain.Menu;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+@Stateless
 public class MenuRepository {
+    @Inject
+    private CrudGenericService crudgeneric;
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     *
+     * @param id busca especificamente en el menu
+     * @return
+     */
+    public List<Menu> findAll(Long id) {
+        String jpql = "select m from Menu m where m.id = :id";
+        TypedQuery<Menu> query = em.createQuery(jpql, Menu.class);
+        query.setParameter("id", id);
+        return query.getResultList();
+    }
+
 }
